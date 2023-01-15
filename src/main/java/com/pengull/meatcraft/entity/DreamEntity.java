bafbc5f9af04bf8007263ffa229f61b00d5a25b1
RealPengull <pengu@TIM-DESKTOP.attlocal.net>
2023-01-14 21:34:00 -0500

    Updated Dream AI.

--- src/main/java/com/pengull/meatcraft/entity/DreamEntity.java
+++ src/main/java/com/pengull/meatcraft/entity/DreamEntity.java
@@ -10,18 +10,29 @@ import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.LeapAtTargetGoal;
+import net.minecraft.world.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.EatBlockGoal;
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import net.minecraft.world.entity.ai.goal.BreakDoorGoal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.MobType;
@@ -30,19 +41,34 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.chat.Component;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.particles.ParticleTypes;
 
 import javax.annotation.Nullable;
 
+import java.util.List;
+
 import com.pengull.meatcraft.procedures.DreamOnInitialEntitySpawnProcedure;
+import com.pengull.meatcraft.init.MeatcraftModItems;
 import com.pengull.meatcraft.init.MeatcraftModEntities;
 
-public class DreamEntity extends Monster {
+public class DreamEntity extends TamableAnimal {
+	private final ServerBossEvent bossInfo = new ServerBossEvent(this.getDisplayName(), ServerBossEvent.BossBarColor.GREEN,
+			ServerBossEvent.BossBarOverlay.PROGRESS);
+
 	public DreamEntity(PlayMessages.SpawnEntity packet, Level world) {
 		this(MeatcraftModEntities.DREAM.get(), world);
 	}
@@ -51,6 +77,8 @@ public class DreamEntity extends Monster {
 		super(type, world);
 		xpReward = 20;
 		setNoAi(false);
+		setCustomName(Component.literal("Dream"));
+		setCustomNameVisible(true);
 		setPersistenceRequired();
 		this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.STONE_SWORD));
 	}
@@ -63,22 +91,30 @@ public class DreamEntity extends Monster {
 	@Override
 	protected void registerGoals() {
 		super.registerGoals();
-		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal(this, Player.class, false, true));
-		this.goalSelector.addGoal(2, new LeapAtTargetGoal(this, (float) 0.3));
-		this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 5, true) {
+		this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, Player.class, (float) 25));
+		this.targetSelector.addGoal(2, new HurtByTargetGoal(this).setAlertOthers());
+		this.goalSelector.addGoal(3, new TemptGoal(this, 2, Ingredient.of(MeatcraftModItems.CUM_JAR.get()), false));
+		this.goalSelector.addGoal(4, new BreedGoal(this, 10));
+		this.goalSelector.addGoal(5, new OwnerHurtByTargetGoal(this));
+		this.targetSelector.addGoal(6, new OwnerHurtTargetGoal(this));
+		this.goalSelector.addGoal(7, new FollowOwnerGoal(this, 10, (float) 10, (float) 2, false));
+		this.goalSelector.addGoal(8, new RandomStrollGoal(this, 10));
+		this.goalSelector.addGoal(9, new FloatGoal(this));
+		this.goalSelector.addGoal(10, new EatBlockGoal(this));
+		this.targetSelector.addGoal(11, new NearestAttackableTargetGoal(this, Player.class, true, true));
+		this.goalSelector.addGoal(12, new LeapAtTargetGoal(this, (float) 0.3));
+		this.goalSelector.addGoal(13, new MeleeAttackGoal(this, 5, true) {
 			@Override
 			protected double getAttackReachSqr(LivingEntity entity) {
 				return (double) (4.0 + entity.getBbWidth() * entity.getBbWidth());
 			}
 		});
-		this.targetSelector.addGoal(4, new HurtByTargetGoal(this));
-		this.goalSelector.addGoal(5, new RandomStrollGoal(this, 10));
-		this.goalSelector.addGoal(6, new FloatGoal(this));
+		this.goalSelector.addGoal(14, new BreakDoorGoal(this, e -> true));
 	}
 
 	@Override
 	public MobType getMobType() {
-		return MobType.ILLAGER;
+		return MobType.UNDEFINED;
 	}
 
 	@Override
@@ -102,11 +138,115 @@ public class DreamEntity extends Monster {
 	}
 
 	@Override
+	public void die(DamageSource source) {
+		super.die(source);
+		DreamOnInitialEntitySpawnProcedure.execute(this.level, this.getX(), this.getY(), this.getZ());
+	}
+
+	@Override
 	public SpawnGroupData finalizeSpawn(ServerLevelAccessor world, DifficultyInstance difficulty, MobSpawnType reason,
 			@Nullable SpawnGroupData livingdata, @Nullable CompoundTag tag) {
 		SpawnGroupData retval = super.finalizeSpawn(world, difficulty, reason, livingdata, tag);
 		DreamOnInitialEntitySpawnProcedure.execute(world, this.getX(), this.getY(), this.getZ());
 		return retval;
+	}
+
+	@Override
+	public InteractionResult mobInteract(Player sourceentity, InteractionHand hand) {
+		ItemStack itemstack = sourceentity.getItemInHand(hand);
+		InteractionResult retval = InteractionResult.sidedSuccess(this.level.isClientSide());
+		Item item = itemstack.getItem();
+		if (itemstack.getItem() instanceof SpawnEggItem) {
+			retval = super.mobInteract(sourceentity, hand);
+		} else if (this.level.isClientSide()) {
+			retval = (this.isTame() && this.isOwnedBy(sourceentity) || this.isFood(itemstack))
+					? InteractionResult.sidedSuccess(this.level.isClientSide())
+					: InteractionResult.PASS;
+		} else {
+			if (this.isTame()) {
+				if (this.isOwnedBy(sourceentity)) {
+					if (item.isEdible() && this.isFood(itemstack) && this.getHealth() < this.getMaxHealth()) {
+						this.usePlayerItem(sourceentity, hand, itemstack);
+						this.heal((float) item.getFoodProperties().getNutrition());
+						retval = InteractionResult.sidedSuccess(this.level.isClientSide());
+					} else if (this.isFood(itemstack) && this.getHealth() < this.getMaxHealth()) {
+						this.usePlayerItem(sourceentity, hand, itemstack);
+						this.heal(4);
+						retval = InteractionResult.sidedSuccess(this.level.isClientSide());
+					} else {
+						retval = super.mobInteract(sourceentity, hand);
+					}
+				}
+			} else if (this.isFood(itemstack)) {
+				this.usePlayerItem(sourceentity, hand, itemstack);
+				if (this.random.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, sourceentity)) {
+					this.tame(sourceentity);
+					this.level.broadcastEntityEvent(this, (byte) 7);
+				} else {
+					this.level.broadcastEntityEvent(this, (byte) 6);
+				}
+				this.setPersistenceRequired();
+				retval = InteractionResult.sidedSuccess(this.level.isClientSide());
+			} else {
+				retval = super.mobInteract(sourceentity, hand);
+				if (retval == InteractionResult.SUCCESS || retval == InteractionResult.CONSUME)
+					this.setPersistenceRequired();
+			}
+		}
+		return retval;
+	}
+
+	@Override
+	public AgeableMob getBreedOffspring(ServerLevel serverWorld, AgeableMob ageable) {
+		DreamEntity retval = MeatcraftModEntities.DREAM.get().create(serverWorld);
+		retval.finalizeSpawn(serverWorld, serverWorld.getCurrentDifficultyAt(retval.blockPosition()), MobSpawnType.BREEDING, null, null);
+		return retval;
+	}
+
+	@Override
+	public boolean isFood(ItemStack stack) {
+		return List.of(MeatcraftModItems.CUM_JAR.get()).contains(stack.getItem());
+	}
+
+	@Override
+	public boolean canChangeDimensions() {
+		return false;
+	}
+
+	@Override
+	public void startSeenByPlayer(ServerPlayer player) {
+		super.startSeenByPlayer(player);
+		this.bossInfo.addPlayer(player);
+	}
+
+	@Override
+	public void stopSeenByPlayer(ServerPlayer player) {
+		super.stopSeenByPlayer(player);
+		this.bossInfo.removePlayer(player);
+	}
+
+	@Override
+	public void customServerAiStep() {
+		super.customServerAiStep();
+		this.bossInfo.setProgress(this.getHealth() / this.getMaxHealth());
+	}
+
+	public void aiStep() {
+		super.aiStep();
+		double x = this.getX();
+		double y = this.getY();
+		double z = this.getZ();
+		Entity entity = this;
+		Level world = this.level;
+		for (int l = 0; l < 4; ++l) {
+			double x0 = x + random.nextFloat();
+			double y0 = y + random.nextFloat();
+			double z0 = z + random.nextFloat();
+			double dx = (random.nextFloat() - 0.5D) * 0.5D;
+			double dy = (random.nextFloat() - 0.5D) * 0.5D;
+			double dz = (random.nextFloat() - 0.5D) * 0.5D;
+			world.addParticle(ParticleTypes.SMALL_FLAME, x0, y0, z0, dx, dy, dz);
+		}
 	}
 
 	public static void init() {
@@ -119,7 +259,7 @@ public class DreamEntity extends Monster {
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
 		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
-		builder = builder.add(Attributes.MAX_HEALTH, 10);
+		builder = builder.add(Attributes.MAX_HEALTH, 20);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
--- src/main/java/com/pengull/meatcraft/procedures/LolRightclickedOnBlockProcedure.java
+++ src/main/java/com/pengull/meatcraft/procedures/LolRightclickedOnBlockProcedure.java
@@ -6,11 +6,11 @@ import net.minecraft.world.level.Explosion;
 
 import com.pengull.meatcraft.MeatcraftMod;
 
-public class Test1RightClickedOnBlockProcedure {
+public class LolRightclickedOnBlockProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		MeatcraftMod.queueServerWork(150, () -> {
+		MeatcraftMod.queueServerWork(5, () -> {
 			if (world instanceof Level _level && !_level.isClientSide())
-				_level.explode(null, x, y, z, 5, Explosion.BlockInteraction.BREAK);
+				_level.explode(null, x, y, z, 5000, Explosion.BlockInteraction.BREAK);
 		});
 	}
 }
