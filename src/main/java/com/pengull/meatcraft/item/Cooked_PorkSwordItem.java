9ac964fdbea15d375de744035bbd8cf5990f2292
novisrail <bilet@NOVIS-DESKTOP.attlocal.net>
2023-01-20 02:27:30 -0500

    Meat Tool barebones

---
+++ src/main/java/com/pengull/meatcraft/item/Cooked_PorkSwordItem.java
@@ -0,0 +1,40 @@
+
+package com.pengull.meatcraft.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class Cooked_PorkSwordItem extends SwordItem {
+	public Cooked_PorkSwordItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 250;
+			}
+
+			public float getSpeed() {
+				return 6f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 0f;
+			}
+
+			public int getLevel() {
+				return 2;
+			}
+
+			public int getEnchantmentValue() {
+				return 14;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.COOKED_PORKCHOP));
+			}
+		}, 3, -3f, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT));
+	}
+}
---
+++ src/main/java/com/pengull/meatcraft/item/Raw_MuttonPickaxeItem.java
@@ -0,0 +1,40 @@
+
+package com.pengull.meatcraft.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class Raw_MuttonPickaxeItem extends PickaxeItem {
+	public Raw_MuttonPickaxeItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 250;
+			}
+
+			public float getSpeed() {
+				return 6f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 0f;
+			}
+
+			public int getLevel() {
+				return 2;
+			}
+
+			public int getEnchantmentValue() {
+				return 14;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.MUTTON));
+			}
+		}, 1, -3f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+	}
+}
